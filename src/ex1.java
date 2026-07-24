4e22a6546a13d5406bc7e9cefee919f08a599710
Livio David Rocha Arvelos <arveloslivio@gmail.com>
2026-07-24 11:18:33 -0300

    subindo projeto pro github

---
+++ src/ex1.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+void main() {
+    System.out.println("Ola, Mundo");
+    Date relogio = new Date();
+    System.out.println("A data do sistema e: ");
+    System.out.println(relogio);
+}
